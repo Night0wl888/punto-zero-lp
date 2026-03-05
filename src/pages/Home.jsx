6a88c76d19af61f0acced62e53caaa1b66796a6f
felipe samper <felikawa007@gmail.com>
2026-03-05 04:10:47 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import HeroSection from "../components/landing/HeroSection";
-import MemoriesSection from "../components/landing/MemoriesSection";
-import WhatIsSection from "../components/landing/WhatIsSection";
-import PhilosophySection from "../components/landing/PhilosophySection";
-import ComingSoonSection from "../components/landing/ComingSoonSection";
-import AboutSection from "../components/landing/AboutSection";
-import HypeSection from "../components/landing/HypeSection";
-import FooterSection from "../components/landing/FooterSection";
+import HeroSection from "@/components/landing/HeroSection";
+import MemoriesSection from "@/components/landing/MemoriesSection";
+import WhatIsSection from "@/components/landing/WhatIsSection";
+import PhilosophySection from "@/components/landing/PhilosophySection";
+import ComingSoonSection from "@/components/landing/ComingSoonSection";
+import AboutSection from "@/components/landing/AboutSection";
+import HypeSection from "@/components/landing/HypeSection";
+import FooterSection from "@/components/landing/FooterSection";
 
 export default function Home() {
   return (
